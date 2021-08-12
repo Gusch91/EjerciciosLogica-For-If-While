961e975bc48a7a5bfa6b4096d95cca21820afabb
Gustavo Chavol <81936874+Gusch91@users.noreply.github.com>
2021-08-12 17:04:53 -0300

    Update Ejercicio6.js

--- Ejercicio6.js
+++ Ejercicio6.js
@@ -1,4 +1,4 @@
-*/ 6 - Escribe un programa que permita ir introduciendo una serie indeterminadade números mientras su suma no supere 50. 
+/*6 - Escribe un programa que permita ir introduciendo una serie indeterminadade números mientras su suma no supere 50. 
 Cuando esto ocurra, se debe mostrar el total acumulado y el contador de cuantos números se han introducido */
 
 let acum=0;
